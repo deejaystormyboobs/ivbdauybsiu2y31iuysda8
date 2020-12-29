73e46f89883a6dc3cff704a82589a7693bdd59a5
deejaystormyboobs <76745140+deejaystormyboobs@users.noreply.github.com>
2020-12-29 16:06:07 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const fs = require('fs');
-const { getA } = require('./z');
+const { getData, generateA, generateB } = require('./z');
 
 main();
 
 async function main(){
     try { fs.mkdirSync('build'); }catch{}
 
-    let txtA = await getA();
+    const data = await getData();
+
+    let txtA = await generateA(data);
     fs.writeFileSync('build/866.m3u8', txtA);
+    fs.writeFileSync('build/866.m3u8.txt', txtA);
+
+    let txtB = await generateB(data);
+    fs.writeFileSync('build/173.m3u8', txtB);
+    fs.writeFileSync('build/173.m3u8.txt', txtB);
 }
--- z.js
+++ z.js
@@ -6,9 +6,11 @@ const URL = config.URL;
 const userAgent = crypto.criptografar(config.USER_AGENT);
 
 module.exports = {
-    async getA(){
+    async getData(){
         const { status, data } = await axios.get(URL);
-
+        return data;
+    },
+    async generateA(data){
         let result = '';
         result += '#EXTM3U tvg-shift=3\n';
 
@@ -24,5 +26,21 @@ module.exports = {
         }
 
         return result;
-    }
+    },
+    async generateB(data){
+        let result = '';
+        result += '#EXTM3U tvg-shift=3\n';
+
+        let id = 1;
+        for(let canal of data){
+            const url = crypto.descriptografar(canal.Link.trim());
+
+            result += `#EXTINF:-1 tvg-id="id${id}" tvg-name="${canal.Descricao.trim()}" tvg-logo="${canal.Logo.trim()}" tvg-chno="${id}" group-title="${canal.Grupo.trim()}",${canal.Descricao.trim()}\n`;
+            result += `${url}|user-agent=${userAgent};\n`;
+
+            id++;
+        }
+
+        return result;
+    },
 }
